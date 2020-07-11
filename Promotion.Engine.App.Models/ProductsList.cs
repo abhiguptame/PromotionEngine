f3dae1560072521c9aa4d2607a323136a7a08536
Abhishek Kumar Gupta <abhiguptame@gmail.com>
2020-07-11 07:03:42 +0530

    Added Product List Class

---
+++ Promotion.Engine.App.Models/ProductsList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Promotion.Engine.App.Models
+{
+    public class ProductsList
+    {
+        public Dictionary<string, float> skuDictionary = new Dictionary<string, float>();
+        public ProductsList()
+        {
+            skuDictionary.Add("A", 50);
+            skuDictionary.Add("B", 30);
+            skuDictionary.Add("C", 20);
+            skuDictionary.Add("D", 15);
+        }
+    }
+}
