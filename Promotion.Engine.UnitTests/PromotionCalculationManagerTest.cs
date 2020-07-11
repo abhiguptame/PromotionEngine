b3cc16465af3fe4a77ca3efce720dd128aa0856e
Abhishek Kumar Gupta <abhiguptame@gmail.com>
2020-07-11 15:28:08 +0530

    Scenario C Unit Test Case Implementation

--- Promotion.Engine.UnitTests/PromotionCalculationManagerTest.cs
+++ Promotion.Engine.UnitTests/PromotionCalculationManagerTest.cs
@@ -54,6 +54,25 @@ namespace Promotion.Engine.UnitTests
             promotionalPrice.Should().Be(370);
         }
 
+        [Fact]
+        public void Senario_C_Unit_Test()
+        {
+            // Arrange 
+            SalesProductsLineItems salesProductsLineItems = new SalesProductsLineItems();
+            salesProductsLineItems.SalesProductDetails.Add("A", 3);
+            salesProductsLineItems.SalesProductDetails.Add("B", 5);
+            salesProductsLineItems.SalesProductDetails.Add("C", 1);
+            salesProductsLineItems.SalesProductDetails.Add("D", 1);
+
+            var promotionCalculationManager = new PromotionCalculationManager();
+
+            // Act
+            var promotionalPrice = promotionCalculationManager.ApplyPromotion(salesProductsLineItems).Result;
+
+            // Assert
+            promotionalPrice.Should().Be(280);
+        }
+
         #endregion
     }
 }
